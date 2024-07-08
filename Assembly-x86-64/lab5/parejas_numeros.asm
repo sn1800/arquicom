94bd1e8c1f69ec54caaf53b095073ff605cedd7f
Fernando C <107225856+sn1800@users.noreply.github.com>
2024-07-08 00:06:57 +0000

    actualizacion parejas_numeros

--- Assembly-x86-64/lab5/parejas_numeros.asm
+++ Assembly-x86-64/lab5/parejas_numeros.asm
@@ -1,7 +1,9 @@
 section .data
-
+    A dd 1;valor del limite inferior
+    B dd 20;valor del limite superior
+    NUMERO dd 1;valor del numero
 section .bss
-
+    CantParejas resd 1 ; reservar 4 bytes para la cantidad de parejas
 section .text
     global _start
 
