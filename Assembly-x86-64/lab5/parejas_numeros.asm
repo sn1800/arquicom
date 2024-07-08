92fd4d7ca45572659f000f4962ed6f34ea2abd2b
Fernando C <107225856+sn1800@users.noreply.github.com>
2024-07-08 01:22:22 +0000

    cambios en parejas_numeros

--- Assembly-x86-64/lab5/parejas_numeros.asm
+++ Assembly-x86-64/lab5/parejas_numeros.asm
@@ -8,4 +8,25 @@ section .text
     global _start
 
 _start:
-    
+
+mov rcx , 0 ; contador de iterador
+mov rbx , [A]; asignamos al registro rbx el valor de A
+loop:
+    mov rax , rbx
+    mul [NUMERO];y= A*NUMERO
+    cmp rax , [B]
+    jg loop_end
+
+    inc [CantParejas]
+    inc rcx
+
+    cmp rcx , [B]
+    jle loop
+
+loop_end:
+    ; Salida del programa
+    mov rax, 60                ; syscall: exit
+    xor rdi, rdi               ; status 0
+    syscall                    ; llamar al kernel
+.done:
+    ret
